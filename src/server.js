a1dc68d48f1cd3ea6a3a40da06198363cdfdd419
Marco Medina <mmedina.informatica@gmail.com>
2026-02-20 02:29:13 -0400

    rr

--- src/server.js
+++ src/server.js
@@ -1,26 +1,26 @@
-// import './config/env.js';
-// import app from './app.js';
-// import routes from './routes.js';
-// import 'dotenv/config';
+import './config/env.js';
+import app from './app.js';
+import routes from './routes.js';
+import 'dotenv/config';
 
 
-// const PORT = process.env.PORT || 5000;
-// routes(app);
+const PORT = process.env.PORT || 5000;
+routes(app);
 
-// app.listen(PORT, '0.0.0.0',() => {
-//   console.log(`✅ API corriendo en:${PORT}`);
-// });
+app.listen(PORT, '0.0.0.0',() => {
+  console.log(`✅ API corriendo en:${PORT}`);
+});
 
 
 
-import pool from './db/pool.js';
+// import pool from './db/pool.js';
 
-(async () => {
-  try {
-    const conn = await pool.getConnection();
-    console.log("✅ Conectado a MySQL Railway");
-    conn.release();
-  } catch (err) {
-    console.error("❌ Error conectando a MySQL:", err.message);
-  }
-})();
+// (async () => {
+//   try {
+//     const conn = await pool.getConnection();
+//     console.log("✅ Conectado a MySQL Railway");
+//     conn.release();
+//   } catch (err) {
+//     console.error("❌ Error conectando a MySQL:", err.message);
+//   }
+// })();
