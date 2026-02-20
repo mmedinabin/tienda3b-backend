092148b82bd2674958375ee21f453a581abee920
Marco Medina <mmedina.informatica@gmail.com>
2026-02-20 10:50:02 -0400

    image preview

--- src/app.js
+++ src/app.js
@@ -1,6 +1,8 @@
 import express from 'express';
 import cors from 'cors';
 import dotenv from 'dotenv';
+import fs from 'fs'
+import path from 'path'
 
 dotenv.config();
 
@@ -8,13 +10,44 @@ const app = express();
 
 app.use(cors());
 app.use(express.json());
-app.use('/uploads', express.static('uploads'))
+//app.use('/uploads', express.static('uploads'))
+
+
+/* ===============================
+   🔥 INICIALIZAR STORAGE PERSISTENTE
+================================ */
+
+const uploadDir = '/data/uploads/productos'
+const defaultImagePath = path.join(uploadDir, 'default.png')
+
+// Crear carpeta si no existe
+if (!fs.existsSync(uploadDir)) {
+  fs.mkdirSync(uploadDir, { recursive: true })
+}
+
+// Copiar default.png si no existe en volume
+if (!fs.existsSync(defaultImagePath)) {
+  const source = path.join(process.cwd(), 'assets/default.png')
+
+  if (fs.existsSync(source)) {
+    fs.copyFileSync(source, defaultImagePath)
+    console.log('✅ default.png copiado al volume')
+  } else {
+    console.log('⚠ No se encontró assets/default.png')
+  }
+}
+
+
+
+
+
+
+app.use('/uploads', express.static('/data/uploads'))
 
 // app.get('/', (req, res) => {
 //   res.json({ message: 'API Minimarket POS funcionando 🚀' });
 // });
 
-// 🔥 Middleware global de errores
 app.use((err, req, res, next) => {
   console.error("🔥 ERROR GLOBAL:", err);
 
--- src/utils/image.js
+++ src/utils/image.js
@@ -1,10 +1,33 @@
+// import sharp from 'sharp'
+// import fs from 'fs'
+// import path from 'path'
+
+// export const guardarImagenProducto = async (buffer) => {
+//   const filename = `prod_${Date.now()}.png`
+//   const outputPath = path.join('uploads/productos', filename)
+
+//   await sharp(buffer)
+//     .resize(300, 300)
+//     .png({ quality: 80 })
+//     .toFile(outputPath)
+
+//   return filename
+// }
+
+
 import sharp from 'sharp'
 import fs from 'fs'
 import path from 'path'
 
 export const guardarImagenProducto = async (buffer) => {
+  const uploadDir = '/data/uploads/productos'
+
+  if (!fs.existsSync(uploadDir)) {
+    fs.mkdirSync(uploadDir, { recursive: true })
+  }
+
   const filename = `prod_${Date.now()}.png`
-  const outputPath = path.join('uploads/productos', filename)
+  const outputPath = path.join(uploadDir, filename)
 
   await sharp(buffer)
     .resize(300, 300)
@@ -13,3 +36,4 @@ export const guardarImagenProducto = async (buffer) => {
 
   return filename
 }
+
