cc9ee64d2a896b96271f496be50a3470b15dc0cd
Marco Medina <mmedina.informatica@gmail.com>
2026-02-26 00:05:54 -0400

    fix envs

--- src/db/pool.js
+++ src/db/pool.js
@@ -1,41 +1,10 @@
 import mysql from 'mysql2/promise';
 
-//LOCAL + BD LOCAL
-// const pool = mysql.createPool({
-//   host: process.env.DB_HOST,
-//   user: process.env.DB_USER,
-//   password: process.env.DB_PASSWORD || '',
-//   database: process.env.DB_NAME,
-//   waitForConnections: true,
-//   connectionLimit: 10,
-// });
-// export default pool;
-
-//LOCAL + BD NUBE
-// const pool = mysql.createPool({
-//   host: process.env.DB_HOST,
-//   user: process.env.DB_USER,
-//   password: process.env.DB_PASSWORD,
-//   database: process.env.DB_NAME,
-//   port: Number(process.env.DB_PORT),
-//   waitForConnections: true,
-//   connectionLimit: 10,
-// });
-
-// export default pool;
-
-
-//para nube
-// import mysql from 'mysql2/promise';
-
 const pool = mysql.createPool({
   host: process.env.DB_HOST,
   user: process.env.DB_USER,
-  password: process.env.DB_PASSWORD,
+  password: process.env.DB_PASSWORD || '',
   database: process.env.DB_NAME,
-  port: Number(process.env.DB_PORT),
-  waitForConnections: true,
-  connectionLimit: 10,
-});
-
+  port: process.env.DB_PORT ? Number(process.env.DB_PORT) : 3306,
+})
 export default pool;
--- src/server.js
+++ src/server.js
@@ -1,7 +1,6 @@
-import "./config/env.js";
+import 'dotenv/config'
 import app from "./app.js";
 import routes from "./routes.js";
-import "dotenv/config";
 
 const PORT = process.env.PORT || 5000;
 routes(app);
@@ -19,15 +18,3 @@ console.log("==========================");
 app.listen(PORT, "0.0.0.0", () => {
   console.log(`✅ API corriendo en:${PORT}`);
 });
-
-// import pool from './db/pool.js';
-
-// (async () => {
-//   try {
-//     const conn = await pool.getConnection();
-//     console.log("✅ Conectado a MySQL Railway");
-//     conn.release();
-//   } catch (err) {
-//     console.error("❌ Error conectando a MySQL:", err.message);
-//   }
-// })();
